cc468f3e6dc66ffd63047959967a2055fec445f4
tudormorar <tudor.morar14@yahoo.com>
2026-02-23 15:20:13 +0200

    Use environment variables for database credentials

--- Backend/src/main/java/com/gameproject/FightSimulatorAIApplication.java
+++ Backend/src/main/java/com/gameproject/FightSimulatorAIApplication.java
@@ -2,14 +2,13 @@ package com.gameproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class GameProjectApplication {
+public class FightSimulatorAIApplication {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(GameProjectApplication.class, args);
+        SpringApplication.run(FightSimulatorAIApplication.class, args);
     }
 
 }
